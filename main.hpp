68cd9c5d79a1428d14d765d789c24d0196fe2e92
dommiloo <156959975+dommiloo@users.noreply.github.com>
2024-03-11 21:35:18 +0000

    Dominic

--- main.hpp
+++ main.hpp
@@ -13,25 +13,21 @@ int getClosestMed(int, int, int);
 // complete two functions
 int getRandom(void)
 {
-       int random = 1 + (rand() % 9);
+       int random = 2 + (rand() % 9);
        return random;
 }
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
-    
-        double mean = (rdnum1 + rdnum2 + rdnum3) / 3.0;
-        if (((abs(mean - rdnum1)) < (abs(mean - rdnum2))) && ((abs(mean - rdnum1)) < (abs(mean - rdnum3))) )
-        {
+     double mean = (rdnum1 + rdnum2 + rdnum3) / 3.0;
+
+    double diff1 = abs(mean - rdnum1);
+    double diff2 = abs(mean - rdnum2);
+    double diff3 = abs(mean - rdnum3);
+
+    if (diff1 <= diff2 && diff1 <= diff3)
         return rdnum1;
-        }
-        else if (((abs(mean - rdnum2)) < (abs(mean - rdnum1))) && ((abs(mean - rdnum2)) < (abs(mean - rdnum3))) ){
-         return rdnum2;
-        }
-
-        else {
-         return rdnum3; 
-       
-        }
-
-        
+    else if (diff2 <= diff1 && diff2 <= diff3)
+        return rdnum2;
+    else
+        return rdnum3;
 }
